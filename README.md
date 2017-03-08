69e54fa3e711c6d54b906cecc926d2c11ae9b76e
rozie <rozie@poczta.onet.pl>
2017-03-08 20:05:11 +0100

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# single-category-feed-maker-for-Blox
+# Single category feed maker for Blox
+===========
+
+This is simple Python script to extract posts belonging to single category on
+Blox.pl RSS feed. Created, because Blox.pl does not support feeds for a single
+category right now, at least not for new templates.
+
+It fetches original RSS feed, and tries to preserve as much as possible.
+
+Usage
+---------
+* clone this repository
+* pip install -r requirements.txt
+* python rss_from_category_blox.py --url FEED_URL --category CATEGORY |\
+  /bin/sed s'/pubdate/pubDate/g' | /bin/sed 's/lastbuilddate/lastBuildDate/g' > /output.xml
---
+++ rss_from_category_blox.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+import argparse
+from bs4 import BeautifulSoup
+
+
+def main():
+    args = parse_arguments()
+    url = args.url
+    r = requests.get(url)
+    data = r.text
+
+    soup = BeautifulSoup(data)
+
+    # head
+    print('<?xml version="1.0" encoding="utf-8"?>')
+    print('<rss version="2.0">')
+    print('<channel>')
+    print soup.channel.title
+    print soup.channel.link
+    print soup.channel.description
+    print soup.channel.lastbuilddate
+
+    # body
+    for item in soup.findAll("item"):
+        if item.category.text == args.category:
+            print(item)
+
+    # end
+    print('</channel>')
+    print('</rss>')
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Script grabs sitemap, returns URLs')
+
+    parser.add_argument(
+        '--url', required=False,
+        default='http://rozie.blox.pl/rss2',
+        help='RSS URL to fetch')
+    parser.add_argument(
+        '--category', required=False,
+        default='DSP2017',
+        help='category to extract')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    main()
